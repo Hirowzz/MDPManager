a029607142b5de3d54fef99074b0bfd5fbea7eb1
Hirowzz <70280622+Hirowzz@users.noreply.github.com>
2024-11-16 23:48:02 +0100

    Update main.py
    updated comments so thats more readable

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import random
 import json
 import os
 
-# Entrée: taille de mdp demandée / Sortie: mdp aléatoire
 def generateur_mdp(taille):
+    """
+    Génère un mot de passe (mdp) aléatoire.
+    Entrée : taille de mdp demandée par l'utilisateur
+    Sortie : mdp aléatoire et nommé dans un fichier json
+    """
     mdp = ""
     for i in range(taille):
-        mdp = mdp + caracteres[int(random.randint(0, len(caracteres)-1))] # chaque terme du mdp est ajouté après le précédent
-    print(mdp)
+        # Chaque caractère est choisi aléatoirement puis ajouté au mdp
+        mdp = mdp + CARACTERES[int(random.randint(0, len(CARACTERES)-1))]
+    print(f"Le mot de passe généré pour {lieu_mdp} est :\n{mdp}")
 
-    # Ajouter ce nouveau mdp au dictionnaire "donnees" dans le json
+    # Ajoute ce nouveau mdp au dictionnaire "donnees" dans le fichier json
     donnees[lieu_mdp] = mdp
-    with open(fichier_json, "w") as fichier:
+    with open(FICHIER_JSON, "w") as fichier:
         json.dump(donnees, fichier, indent=4)
 
-# Retrouver un mdp précis dans le json en cherchant son nom puis en affichant le mdp attribué
+
 def retrouver_mdp():
+    """
+    Retrouve un mdp dans le fichier json a partir de son nom
+    Entrée : nom donné par l'utilisateur
+    Sortie : mot de passe renseigné à ce nom
+    """
     if lieu_mdp_demande in donnees:
         print(f"Le mot de passe pour {lieu_mdp_demande} est :\n" + donnees[lieu_mdp_demande])
     else:
@@ -23,9 +33,9 @@ def retrouver_mdp():
 
 #------------------ Affectation des variables ------------------
 
-demande = input("------------ Que voulez vous faire ? ------------\n Tapez 'generer' pour générer un nouveau mot de passe \n Tapez 'chercher' pour chercher un nouveau mot de passe\n")
-caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890;:!%/.?&'(-_)=@+$[{#|`^]}µ*"
-fichier_json = "passwords.json"
+DEMANDE = input("------------ Que voulez vous faire ? ------------\n Tapez 'generer' pour générer un nouveau mot de passe \n Tapez 'chercher' pour chercher un nouveau mot de passe\n")
+CARACTERES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890;:!%/.?&'(-_)=@+$[{#|`^]}µ*"
+FICHIER_JSON = "passwords.json"
 
 taille_demandee = ""
 lieu_mdp = ""
@@ -33,23 +43,30 @@ lieu_mdp_demande = ""
 
 #------------------ Programme principal ------------------
 
-if os.path.exists(fichier_json): # Vérifier que le fichier existe
-    with open(fichier_json, "r") as fichier:
+# Vérifier que le fichier existe
+if os.path.exists(FICHIER_JSON):
+    with open(FICHIER_JSON, "r") as fichier:
         contenu = fichier.read().strip()
-        donnees = json.loads(contenu) if contenu else {}  # Charger les données ou initialiser un dictionnaire
+        # Charger les données ou initialiser un dictionnaire
+        donnees = json.loads(contenu) if contenu else {}
 else:
-    donnees = {} # créer un dictionnaire "donnees"
+    # créer un dictionnaire "donnees"
+    donnees = {}
 
-if demande == "generer":
-    taille_demandee = int(input(f"Veuillez choisir une longueur de mot de passe [ MAXIMUM = {len(caracteres) - 1} ]"))
-    lieu_mdp = input("Pour quel compte souhaiterez-vous générer un mot de passe ?")
-    if taille_demandee > len(caracteres)-1:
+if DEMANDE == "generer":
+    taille_demandee = int(input(f"Veuillez choisir une longueur de mot de passe [ MAXIMUM = {len(CARACTERES) - 1} ]"))
+    # Vérifie que la taille demandée ne dépasse pas le nombre de caractères disponibles
+    if taille_demandee > len(CARACTERES)-1:
         print("nombre trop élevé!")
     else:
+        # Demande le nom a attribué au mdp puis le génère
+        lieu_mdp = input("Pour quel compte souhaiterez-vous générer un mot de passe ?")
         generateur_mdp(taille_demandee)
 
-elif demande == "chercher":
+elif DEMANDE == "chercher":
+    # Demande quel mdp veut il récupérer puis le récupère
     lieu_mdp_demande = input("Quel mot de passe souhaitez-vous chercher ?")
     retrouver_mdp()
 
-else: print("Veuillez formuler l'une des 2 commandes proposées")
+else:
+    print("Veuillez formuler l'une des 2 commandes proposées")
